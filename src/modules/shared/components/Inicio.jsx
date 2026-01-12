2c1fe859c96c5766bdb34677b195f3d8f6edd54f
Ignacio <ignacionazar9@gmail.com>
2026-01-12 01:46:32 -0300

    agregue la seccion de contacto

--- src/modules/shared/components/Inicio.jsx
+++ src/modules/shared/components/Inicio.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Restaurant from "../../../assets/Restaurant.jpg";
-import Carousel1 from "../../../assets/carousel.webp";
-import Carousel2 from "../../../assets/carousel2.avif";
+import img1 from "../../../assets/img1.png";
+import img2 from "../../../assets/img2.png";
+import img3 from "../../../assets/img3.png";
+import img4 from "../../../assets/img4.png";
+import img5 from "../../../assets/img5.png";
+import img6 from "../../../assets/img6.png";
+import img7 from "../../../assets/img7.png";
+import img8 from "../../../assets/img8.png";
+import img9 from "../../../assets/img9.png";
 import { useState } from "react";
 
 const Inicio = () => {
   const [active, setActive] = useState(0);
+  const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9];
+  const total = images.length;
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-grow">
@@ -22,7 +31,7 @@ const Inicio = () => {
               />
             </div>
             <div>
-              <p>
+              <p className="text-base">
                 <br />
                 Somos un restaurante moderno que nace con la idea de ofrecer una
                 experiencia gastronómica actual, combinando sabores bien
@@ -59,14 +68,16 @@ const Inicio = () => {
             </div>
           </div>
         </section>
-        <section>
+        <section id="galeria" className="scroll-mt-24">
           <h1 className="text-center my-5 text-3xl font-semibold mb-6">
             Galería
           </h1>
           <div className="max-w-6xl mx-auto px-8 my-5">
             <div className="relative overflow-hidden rounded-xl">
               <button
-                onClick={() => setActive((prev) => (prev === 0 ? 1 : prev - 1))}
+                onClick={() =>
+                  setActive((prev) => (prev === 0 ? total - 1 : prev - 1))
+                }
                 className="absolute left-3 top-1/2 -translate-y-1/2 z-10
                  bg-black/40 text-white p-2 rounded-full
                  hover:bg-black/60 transition"
@@ -74,7 +85,9 @@ const Inicio = () => {
                 ❮
               </button>
               <button
-                onClick={() => setActive((prev) => (prev === 1 ? 0 : prev + 1))}
+                onClick={() =>
+                  setActive((prev) => (prev === total - 1 ? 0 : prev + 1))
+                }
                 className="absolute right-3 top-1/2 -translate-y-1/2 z-10
                  bg-black/40 text-white p-2 rounded-full
                  hover:bg-black/60 transition"
@@ -82,7 +95,7 @@ const Inicio = () => {
                 ❯
               </button>
               <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-2 z-10">
-                {[0, 1].map((i) => (
+                {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
                   <button
                     key={i}
                     onClick={() => setActive(i)}
@@ -96,18 +109,60 @@ const Inicio = () => {
                 className="flex transition-transform duration-500"
                 style={{ transform: `translateX(-${active * 100}%)` }}
               >
-                {[Carousel1, Carousel2].map((img, i) => (
-                  <div
-                    key={i}
-                    className="w-full flex-shrink-0 aspect-video overflow-hidden"
-                  >
-                    <img
-                      src={img}
-                      className="w-full h-full object-cover"
-                      alt={`slide-${i}`}
-                    />
-                  </div>
-                ))}
+                {[img1, img2, img3, img4, img5, img6, img7, img8, img9].map(
+                  (img, i) => (
+                    <div
+                      key={i}
+                      className="w-full flex-shrink-0 aspect-video overflow-hidden"
+                    >
+                      <img
+                        src={img}
+                        className="w-full h-full object-cover"
+                        alt={`slide-${i}`}
+                      />
+                    </div>
+                  )
+                )}
+              </div>
+            </div>
+          </div>
+        </section>
+        <section
+          id="contacto"
+          className="bg-neutral-800 text-white pt-10 pb-15"
+        >
+          <h1 className="text-center my-5 text-3xl font-semibold">Contacto</h1>
+          <div className="container mx-auto px-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+              <div className="flex flex-col items-center">
+                <i className="bi bi-telephone text-4xl mb-4 text-red-400"></i>
+                <h5 className="text-gray-100 font-bold mb-2 text-xl text-center">
+                  Teléfono
+                </h5>
+                <ul className="list-none p-0 text-center">
+                  <li className="mb-1 text-base">+54 11 4567 2890</li>
+                  <li className="mb-1 text-base">+54 11 6234 7812</li>
+                </ul>
+              </div>
+              <div className="flex flex-col items-center">
+                <i className="bi bi-envelope text-4xl mb-4 text-red-400"></i>
+                <h5 className="text-gray-100 font-bold mb-2 text-xl text-center">
+                  Email
+                </h5>
+                <ul className="list-none p-0 text-center">
+                  <li className="mb-1 text-base">info@elbuenbocado.com</li>
+                  <li className="mb-1 text-base">reservas@elbuenbocado.com</li>
+                </ul>
+              </div>
+              <div className="flex flex-col items-center">
+                <i className="bi bi-geo-alt text-4xl mb-4 text-red-400"></i>
+                <h5 className="text-gray-100 font-bold mb-2 text-xl text-center">
+                  Dirección
+                </h5>
+                <ul className="list-none p-0 text-center">
+                  <li className="mb-1 text-base">Av. Mate de Luna 2140</li>
+                  <li className="mb-1 text-base">Tucumán, Argentina</li>
+                </ul>
               </div>
             </div>
           </div>
@@ -132,7 +187,7 @@ const Inicio = () => {
                 </li>
                 <li className="mb-1">
                   <a
-                    href=""
+                    href="#galeria"
                     className="text-gray-100 no-underline text-base hover:text-white"
                   >
                     Galeria
@@ -140,7 +195,7 @@ const Inicio = () => {
                 </li>
                 <li className="mb-1">
                   <a
-                    href=""
+                    href="#contacto"
                     className="text-gray-100 no-underline text-base hover:text-white"
                   >
                     Contacto
